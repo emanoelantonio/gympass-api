494553e6580ae942f6331083c6145d1e9c4b6e06
emanoelantonio <dev.emanoel@protonmail.com>
2024-09-09 10:12:48 -0300

    refactor(register): add password hash and validation

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,5 @@
 import { prisma } from '@/lib/prisma';
+import { hash } from 'bcryptjs';
 import type { FastifyReply, FastifyRequest } from 'fastify';
 import { z } from 'zod';
 
@@ -10,12 +11,23 @@ export async function register(request: FastifyRequest, replay: FastifyReply) {
 	});
 
 	const { name, email, password } = registerBodySchema.parse(request.body);
+	const password_hash = await hash(password, 6);
+
+	const userWithSameEmail = await prisma.user.findUnique({
+		where: {
+			email,
+		},
+	});
+
+	if (userWithSameEmail) {
+		return replay.status(409).send();
+	}
 
 	await prisma.user.create({
 		data: {
 			name,
 			email,
-			password_hash: password,
+			password_hash,
 		},
 	});
 
